9c347f310f967368e0d3b5504b4a652eadb1cd82
Sahana Sivanandarajah <81416603+sahanas1998@users.noreply.github.com>
2023-06-20 23:38:31 +0530

    up

--- src/Components/Home/Main.js
+++ src/Components/Home/Main.js
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import SendIcon from '@mui/icons-material/Send';
 import CheckCircleSharpIcon from '@mui/icons-material/CheckCircleSharp';
 
-const CardItem = ({ title, description, color , action }) => {
+const CardItem = ({ title, description, color , action , size }) => {
   return (
     <Card sx={{ minWidth: 485, minHeight: 200, borderRadius: 8, backgroundColor: color, color: "#f3e5f5" , marginTop:4 , padding:4}}>
       <CardContent>
@@ -16,7 +16,7 @@ const CardItem = ({ title, description, color , action }) => {
           {title}
         </Typography>
         <br/>
-        <Typography variant="p">
+        <Typography variant="p" class={size} >
           {description}
         </Typography>
       </CardContent>
@@ -38,14 +38,14 @@ const Main = () => {
         `}
       </style>
       <div className="flex flex-row text-left space-x-16" >
-        <CardItem title="Set Delivery" description="Send orders quickly and easily by setting delivery services now" color="#9c27b0" action={ <Button endIcon={<SendIcon/>} sx={{ backgroundColor: '#f3e5f5', borderRadius: 7, color: "#9c27b0", padding: 1.5 }}>
+        <CardItem title="Set Delivery" description="Send orders quickly and easily by setting delivery services now" color="#9c27b0" size="p font-thin" action={ <Button endIcon={<SendIcon/>} sx={{ backgroundColor: '#f3e5f5', borderRadius: 7, color: "#9c27b0", padding: 1.5 }}>
   Fill in this detail
        </Button>} />
-        <CardItem title="Set Bank Account" description="Cash but your earning earsily by selling your bank account information" color="#512da8" action={<div class='flex flex-row  gap-2 pl-4'>
+        <CardItem title="Set Bank Account" description="Cash but your earning earsily by selling your bank account information" color="#512da8" size="text-xs font-thin" action={<div class='flex flex-row  gap-2 pl-4'>
        <CheckCircleSharpIcon/>
        <h4>Completed</h4>
        </div>} />
-        <CardItem title="Update your Profile" description="The faster you sell, the faster you'll get the thing. can update your profile" color="#512da8" action={<div class='flex flex-row  gap-2 pl-4'>
+        <CardItem title="Update your Profile" description="The faster you sell, the faster you'll get the thing. can update your profile" color="#512da8"  size="text-xs font-thin" action={<div class='flex flex-row  gap-2 pl-4'>
        <CheckCircleSharpIcon/>
        <h4>Completed</h4>
        </div>} />
