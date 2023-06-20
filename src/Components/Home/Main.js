02c99af269594e776c61eeaa123317427bc24fee
Sahana Sivanandarajah <81416603+sahanas1998@users.noreply.github.com>
2023-06-20 18:46:51 +0530

    update

--- src/Components/Home/Main.js
+++ src/Components/Home/Main.js
@@ -6,8 +6,9 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import SendIcon from '@mui/icons-material/Send';
+import CheckCircleSharpIcon from '@mui/icons-material/CheckCircleSharp';
 
-const CardItem = ({ title, description, color }) => {
+const CardItem = ({ title, description, color , action }) => {
   return (
     <Card sx={{ minWidth: 485, minHeight: 200, borderRadius: 8, backgroundColor: color, color: "#f3e5f5" , marginTop:4 , padding:4}}>
       <CardContent>
@@ -19,10 +20,8 @@ const CardItem = ({ title, description, color }) => {
           {description}
         </Typography>
       </CardContent>
-      <CardActions>
-      <Button endIcon={<SendIcon sx={{ color: color }} />} sx={{ backgroundColor: '#f3e5f5', borderRadius: 7, color: color, padding: 2 }}>
-  Fill in this detail
-</Button>
+      <CardActions >
+        {action}
       </CardActions>
     </Card>
   );
@@ -39,9 +38,17 @@ const Main = () => {
         `}
       </style>
       <div className="flex flex-row text-left space-x-16" >
-        <CardItem title="Set Delivery" description="Send orders quickly and easily by setting delivery services now" color="#9c27b0" />
-        <CardItem title="Set Bankdddd Account" description="Cash but your earning earsily by selling your bank account information" color="#512da8" />
-        <CardItem title="Update your Profile" description="The faster you sell, the faster you'll get the thing. can update your profile" color="#512da8" />
+        <CardItem title="Set Delivery" description="Send orders quickly and easily by setting delivery services now" color="#9c27b0" action={ <Button endIcon={<SendIcon/>} sx={{ backgroundColor: '#f3e5f5', borderRadius: 7, color: "#9c27b0", padding: 1.5 }}>
+  Fill in this detail
+       </Button>} />
+        <CardItem title="Set Bankdddd Account" description="Cash but your earning earsily by selling your bank account information" color="#512da8" action={<div class='flex flex-row  gap-2 pl-4'>
+       <CheckCircleSharpIcon/>
+       <h4>Completed</h4>
+       </div>} />
+        <CardItem title="Update your Profile" description="The faster you sell, the faster you'll get the thing. can update your profile" color="#512da8" action={<div class='flex flex-row  gap-2 pl-4'>
+       <CheckCircleSharpIcon/>
+       <h4>Completed</h4>
+       </div>} />
       </div>
     </div>
   );
